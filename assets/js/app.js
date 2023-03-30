c588b6108121599b1f02c3ff10239b29644c3cd2
0x6D7366 <98208009+0x6D7366@users.noreply.github.com>
2023-03-31 01:15:41 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,9 @@
 import { Game } from "./game.js";
 
-document.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener('DOMContentLoaded', event => {
     const app = document.body;
-
-    app.id = 'app';
-
-    // create a TicTacToe Game
+    app.setAttribute('id', 'app');
+    
     const game = new Game(app);
-
     game.start();
-
 });
-
-document.addEventListener('contextmenu', event => event.preventDefault(), false);
-
-document.addEventListener('keydown', event => {
-    if (event.code != "F11") event.preventDefault();
-}, false);
